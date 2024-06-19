c66e75012d16ff6c54cba3edbbbfb7cc165563bb
Pablo Schaffner <pablo@puntorigen.com>
2024-06-19 14:05:43 -0400

    - deactivated repo vis workflow

--- web/src/pages/HomePage.jsx
+++ web/src/pages/HomePage.jsx
@@ -1,17 +1,10 @@
 import React, { useRef, useState } from 'react';
 import { WiredCard, WiredButton, WiredInput } from 'react-wired-elements';
-import { brandSchema, brochureSchema, privacyPolicy } from '../schemas';
-import { tools } from '../experts/constants';
-
-import Meeting from '../components/Meeting';
-import AccountManager from '../experts/AccountManager';
-import Designer from '../experts/Designer';
-import Lawyer from '../experts/Lawyer';
-import ResearchAnalyst from '../experts/ResearchAnalyst';
-import LeadMarketAnalyst from '../experts/Marketing/LeadMarketAnalyst';
 
 function HomePage() {
-    return (<h1>Home Page</h1>);
-}
+    return (
+        <h1 style={{ color:'white' }}>Choose an Example :)</h1>
+    );
+} 
 
 export default HomePage;
